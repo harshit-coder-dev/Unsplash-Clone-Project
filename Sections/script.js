0be1b9aa8ec3349eae5ed1b9926c59f357537819
Sahil Bhaskar <sahilbhaskar40@gmail.com>
2022-06-19 02:55:22 +0530

    All done

---
+++ Sections/script.js
@@ -0,0 +1,23 @@
+document
+	.querySelector(".fa-hamburger-menu")
+	.addEventListener("click", function () {
+		document.querySelector("#menu").classList.toggle("visible");
+	});
+
+const currentUser = localStorage.getItem("name");
+
+if (currentUser) {
+	const userNameElem = document.querySelector("#user-name");
+	userNameElem.style.display = "inline";
+	userNameElem.innerText = currentUser;
+	const profilePhoto = document.querySelector("#profile-photo");
+	profilePhoto.style.display = "inline-block";
+	document.querySelector("#vertical-bar").style.display = "none";
+	document.querySelector(
+		"a[href='../Signup & Login/Login.html']"
+	).style.display = "none";
+	document.querySelector("#forward-slash").style.display = "none";
+	document.querySelector(
+		"a[href='../Signup & Login/SignUp.html']"
+	).style.display = "none";
+}
